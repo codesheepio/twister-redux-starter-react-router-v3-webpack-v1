dcfc2d6206d9eebcadacd9437dcdbe06b8a143bc
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-10-02 02:19:07 +0700

    Add Tweet Component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import customStyle from './styles/custom.scss'
 import mainStyle from './styles/main.scss'
+import Tweet from './components/Tweet'
 
 const App = () => (
-  <div className='container'>
-    <div className='jumbotron'>
-      <h1 style={{ color: '#000' }}>Hello, world!</h1>
-      <p>...</p>
-      <p><a className='btn btn-primary btn-lg' href='#' role='button'>Learn more</a></p>
-    </div>
-  </div>
+  <Tweet
+    name={'Supasate Choochaisri'}
+    screenName={'kaizerwing'}
+    tweetText={'Lorem ipsum'}
+  />
 )
 ReactDOM.render(<App />, document.getElementById('react-root'))
