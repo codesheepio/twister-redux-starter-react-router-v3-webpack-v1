dff1f38b18048e30b3649595f057a351c0f3376c
Arnupharp Viratanapanu <Arnupharp Viratanapanu>
2016-10-02 14:40:49 +0700

    Change screenName -> username

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,16 +1,16 @@
 import React, { PropTypes } from 'react'
 
-const Tweet = ({ name, screenName, tweetText }) => (
+const Tweet = ({ name, username, tweetText }) => (
   <div className={'tweet'}>
     <div className={'name'}>{ name }</div>
-    <div className={'screen-name'}>@{ screenName }</div>
+    <div className={'screen-name'}>@{ username }</div>
     <div className={'tweet-text'}>{ tweetText }</div>
   </div>
 )
 
 Tweet.propTypes = {
   name: PropTypes.string.isRequired,
-  screenName: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
   tweetText: PropTypes.string.isRequired,
 }
 
