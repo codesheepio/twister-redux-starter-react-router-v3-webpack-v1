4c0ba4732fa04fe03601cd9f8c86ab32637d545d
Arnupharp Viratanapanu <Arnupharp Viratanapanu>
2016-10-02 17:30:53 +0700

    Update NewTweet component

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,23 +1,81 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 
-const NewTweet = () => (
-  <div className={'new-tweet'}>
-    <form className={'form-horizontal'}>
-      <div className={'form-group'}>
-        <div className={'tweet-text col-sm-10'}>
-          <input
-            type={'text'}
-            id={'tweetText'}
-            className={'form-control'}
-            placeholder={`What's happening`}
-          />
-        </div>
-        <div className={'col-sm-2'}>
-          <input type={'button'} className={'btn btn-default'} value={'Tweet'} />
-        </div>
+class NewTweet extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tweetText: '',
+    }
+    this.changeTweetText = this.changeTweetText.bind(this)
+    this.addTweet = this.addTweet.bind(this)
+  }
+  changeTweetText(event) {
+    this.setState({
+      tweetText: e.target.value,
+    })
+  }
+  addTweet(event) {
+    fetch('http://localhost:3000/api/tweets', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: this.props.name,
+        username: this.props.username,
+        tweetText: this.state.tweetText,
+      })
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      this.setState({
+        tweetText: '',
+      })
+      this.props.addToTweetList(data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+  render() {
+    return (
+      <div className={'new-tweet'}>
+        <form className={'form-horizontal'}>
+          <div className={'form-group'}>
+            <div className={'tweet-text col-sm-10'}>
+              <input
+                type={'text'}
+                id={'tweetText'}
+                className={'form-control'}
+                placeholder={`What's happening`}
+                onChange={ this.changeTweetText }
+                value={ this.state.tweetText }
+              />
+            </div>
+            <div className={'col-sm-2'}>
+              <input
+                type={'button'}
+                className={'btn btn-default'}
+                value={'Tweet'}
+                onClick={ this.addTweet }
+              />
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
-)
+    )
+  }
+}
 
+NewTweet.propTypes = {
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  addToTweetList: PropTypes.func,
+}
 export default NewTweet
