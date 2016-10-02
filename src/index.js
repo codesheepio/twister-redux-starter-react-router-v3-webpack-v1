b841a570865bbb6bea249fb06362536afb680a22
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-10-02 20:51:24 +0700

    Add Tweet to backend

--- src/index.js
+++ src/index.js
@@ -2,27 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import customStyle from './styles/custom.scss'
 import mainStyle from './styles/main.scss'
-import TweetList from './components/TweetList'
-import NewTweet from './components/NewTweet'
+import MainPanel from './components/MainPanel'
 
-const tweets = [
-  {
-    id: 0,
-    name: 'Supasate Choochaisri',
-    username: 'kaizerwing',
-    tweetText: 'Lorem Ipsum ...' 
-  },
-  {
-    id: 1,
-    name: 'Arnupharp Viratanapanu',
-    username: 'topscores',
-    tweetText: 'Lorem Ipsum ...' 
-  },
-]
 const App = () => (
-  <div>
-    <NewTweet name={'Arnupharp'} username={'topscores'} />
-    <TweetList tweets={tweets} />
-  </div>
+  <MainPanel />
 )
 ReactDOM.render(<App />, document.getElementById('react-root'))
