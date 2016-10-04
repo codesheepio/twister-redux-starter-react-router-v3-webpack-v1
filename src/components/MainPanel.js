4c8f2a7db47142d80ef3b658741d0d60b4871626
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-04 21:10:18 +0700

    Use container

--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import NewTweet from './NewTweet'
-import TweetList from './TweetList'
+import TweetList from '../containers/TweetList'
 
 const MainPanel = ({ enableTweet }) => (
   <div className="main-panel">
