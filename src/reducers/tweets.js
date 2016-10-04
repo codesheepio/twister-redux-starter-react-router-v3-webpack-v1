13878d50438c7226595e5a5e14533db6b0039670
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-04 20:05:53 +0700

    Create tweets reducer

---
+++ src/reducers/tweets.js
@@ -0,0 +1,16 @@
+import { FETCH_TWEETS_SUCCESS } from '../actions/types'
+
+const initialState = []
+
+const tweetsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_TWEETS_SUCCESS: {
+      return action.payload.tweets,
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+export default tweetsReducer
