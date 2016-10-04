6ff31e93b594101105703738a21e1b8a806c1fec
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-05 01:09:13 +0700

    Attach redux-router state to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
+import { routerStateReducer } from 'redux-router'
 import tweetsReducer from './tweets'
 
 const rootReducer = combineReducers({
+  router: routerStateReducer,
   tweets: tweetsReducer,
 })
 
