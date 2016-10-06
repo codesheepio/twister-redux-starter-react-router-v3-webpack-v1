aec03b9c6b134e552ee90f48ec812e2a144c44be
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-07 00:05:50 +0700

    Load state from local storage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import customStyle from './styles/custom.scss'
-import mainStyle from './styles/main.scss'
 import { ReduxRouter, reduxReactRouter } from 'redux-router'
 import { createHistory } from 'history'
 import { createStore, applyMiddleware, compose } from 'redux'
@@ -9,9 +7,15 @@ import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import routes from './routes'
+import { loadState } from './utils/localStorage'
+import './styles/custom.scss'
+import './styles/main.scss'
+
+const preloadedState = loadState()
 
 const store = createStore(
   rootReducer,
+  preloadedState,
   compose(
     applyMiddleware(thunk),
     reduxReactRouter({
---
+++ src/utils/localStorage.js
@@ -0,0 +1,16 @@
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state')
+
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch (err) {
+    return undefined
+  }
+}
+
+export {
+  loadState,
+}
