f0b5405e41bc665bc147ec906c8d5e22b5998c26
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-06 23:17:10 +0700

    Add route to LoginForm

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import MainLayout from '../layouts/MainLayout'
 import BodyContainer from '../components/BodyContainer'
+import LoginForm from '../containers/LoginForm'
 
 const routes = (
-  <Route path='/' component={MainLayout}>
+  <Route path="/" component={MainLayout}>
     <IndexRoute component={BodyContainer} />
-    <Route path=':ownerUsername' component={BodyContainer} />
+    <Route path="login" component={LoginForm} />
+    <Route path=":ownerUsername" component={BodyContainer} />
   </Route>
 )
 
