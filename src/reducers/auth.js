33bd353296f5ab1a366f1bd3b515dda10762fa40
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-06 22:43:26 +0700

    Create authReducer and attach to rootReducer

---
+++ src/reducers/auth.js
@@ -0,0 +1,24 @@
+import { AUTH_LOGIN_SUCCESS } from '../actions/types'
+
+const initialState = {
+  name: '',
+  username: '',
+  token: '',
+}
+
+const authReducer = (state = initialState, action) => ({
+  switch (action.type) {
+    case AUTH_LOGIN_SUCCESS: {
+      return {
+        name: action.payload.name,
+        username: action.payload.username,
+        token: action.payload:token,
+      }
+    }
+    default {
+      return state
+    }
+  }
+})
+
+export default authReducer
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { routerStateReducer } from 'redux-router'
 import tweetsReducer from './tweets'
+import authReducer from './auth'
 
 const rootReducer = combineReducers({
   router: routerStateReducer,
   tweets: tweetsReducer,
+  auth: authReducer,
 })
 
 export default rootReducer
