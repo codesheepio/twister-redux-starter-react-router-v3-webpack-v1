07c92be5c50d2f795b9f607ada00efd8f2c02a86
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-31 20:25:24 +0700

    Create signup thunk action and connect to SignupForm

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,4 @@
+import { push } from 'redux-router'
 import { AUTH_LOGIN_SUCCESS } from './types'
 
 const loginSuccess = (username, name, token) => ({
@@ -21,7 +22,7 @@ const login = (username, password) => (dispatch) => {
     body: JSON.stringify({
       username,
       password,
-    })
+    }),
   })
   .then((response) => {
     if (!response.ok) {
@@ -29,10 +30,37 @@ const login = (username, password) => (dispatch) => {
     }
     return response.json()
   })
-  .then((authInfo) => dispatch(loginSuccess(authInfo.username, authInfo.name, authInfo.token)))
-  .catch((err) => console.error(err))
+  .then(authInfo => dispatch(loginSuccess(authInfo.username, authInfo.name, authInfo.token)))
+  .catch(err => console.error(err))
+}
+
+const signup = (username, name, email, password) => (dispatch) => {
+  const uri = 'http://localhost:3000/api/TwisterUsers'
+  console.log('signup')
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      name,
+      email,
+      password,
+    }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then(() => dispatch(push('/')))
+  .catch(err => console.err(err))
 }
 
 export {
   login,
+  signup,
 }
--- src/containers/SignupForm.js
+++ src/containers/SignupForm.js
@@ -1,59 +1,77 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { signup } from '../actions/auth'
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props)
+    this.submitSignup = this.submitSignup.bind(this)
+  }
+
+  submitSignup(values) {
+    this.props.signup(values.username, values.name, values.email, values.password)
+  }
+
   render() {
     return (
       <div className="signup-form">
-        <div className="logo text-center">
-          Sign up
-        </div>
-        <div className="form-group">
-          <Field
-            name="username"
-            component="input"
-            type="text"
-            className="form-control"
-            placeholder="username"
-          />
-        </div>
-        <div className="form-group">
-          <Field
-            name="fullname"
-            component="input"
-            type="text"
-            className="form-control"
-            placeholder="Firstname Lastname"
-          />
-        </div>
-        <div className="form-group">
-          <Field
-            name="email"
-            component="input"
-            type="email"
-            className="form-control"
-            placeholder="Email"
-          />
-        </div>
-        <div className="form-group">
-          <Field
-            name="password"
-            component="input"
-            type="password"
-            className="form-control"
-            placeholder="password"
-          />
-        </div>
-        <div className="form-group text-right">
-          <button className="btn btn-primary">Sign up</button>
-        </div>
+        <form onSubmit={this.props.handleSubmit(this.submitSignup)}>
+          <div className="logo text-center">
+            Sign up
+          </div>
+          <div className="form-group">
+            <Field
+              name="username"
+              component="input"
+              type="text"
+              className="form-control"
+              placeholder="username"
+            />
+          </div>
+          <div className="form-group">
+            <Field
+              name="name"
+              component="input"
+              type="text"
+              className="form-control"
+              placeholder="Firstname Lastname"
+            />
+          </div>
+          <div className="form-group">
+            <Field
+              name="email"
+              component="input"
+              type="email"
+              className="form-control"
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group">
+            <Field
+              name="password"
+              component="input"
+              type="password"
+              className="form-control"
+              placeholder="password"
+            />
+          </div>
+          <div className="form-group text-right">
+            <button className="btn btn-primary">Sign up</button>
+          </div>
+        </form>
       </div>
     )
   }
+}
+
+SignupForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  signup: PropTypes.func.isRequired,
 }
 
 const ReduxSignupForm = reduxForm({
   form: 'signup',
 })(SignupForm)
 
-export default ReduxSignupForm
+export default connect(null, { signup })(ReduxSignupForm)
