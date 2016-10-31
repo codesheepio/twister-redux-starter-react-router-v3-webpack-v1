01b1833af33ae5d1b98d75b2aceff3ef33ac7736
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-31 18:30:57 +0700

    Add route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router'
 import MainLayout from '../layouts/MainLayout'
 import BodyContainer from '../components/BodyContainer'
 import LoginForm from '../containers/LoginForm'
+import SignupForm from '../containers/SignupForm'
 
 const routes = (
   <Route path="/" component={MainLayout}>
     <IndexRoute component={BodyContainer} />
     <Route path="login" component={LoginForm} />
+    <Route path="signup" component={SignupForm} />
     <Route path=":ownerUsername" component={BodyContainer} />
   </Route>
 )
