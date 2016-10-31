7fcba9ba3ec912937e4bc6cb9cf1f08055043b16
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-31 23:39:09 +0700

    Link to log in and sign up page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
 
-const NavBar = () => (
-  <nav className="navbar navbar-default navbar-fixed-top">
-    <div className="container">
-      <div className="navbar-header">
-        <button
-          type="button"
-          className="navbar-toggle collapsed"
-          data-toggle="collapse"
-          data-target="#bs-example-navbar-collapse-1"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-        </button>
-        <a className="navbar-brand" href="#">Twister </a>
-      </div>
+const NavBar = (props) => {
+  const handleLogout = () => {
+    props.logout()
+  }
+
+  const authButtons = props.token ?
+    <div className="navbar-form navbar-right">
+      <button className="btn btn-primary" onClick={handleLogout}>Log out</button>
     </div>
-  </nav>
-)
+    :
+    <div className="navbar-form navbar-right">
+      <Link to="/login" className="btn btn-default">Log in</Link>
+      <Link to="/signup" className="btn btn-primary">Sign up</Link>
+    </div>
+
+  return (
+    <nav className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <Link to="/" className="navbar-brand">Twister</Link>
+        </div>
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          { authButtons }
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+NavBar.propTypes = {
+  token: PropTypes.string,
+  logout: PropTypes.func.isRequired,
+}
 
 export default NavBar
