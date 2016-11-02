3e068d33d1f8916baba31ddf5297d085ca36f4b9
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-02 21:16:04 +0700

    Create postTweet thunk action

--- src/actions/tweet.js
+++ src/actions/tweet.js
@@ -1,13 +1,13 @@
-import { FETCH_TWEETS_SUCCESS } from './types'
+import { FETCH_TWEETS_SUCCESS, TWEET_POST_SUCCESS } from './types'
 
-const fetchTweetsSuccess = (tweets) => ({
+const fetchTweetsSuccess = tweets => ({
   type: FETCH_TWEETS_SUCCESS,
   payload: {
     tweets,
-  }
+  },
 })
 
-const fetchTweets = (username) => (dispatch) => {
+const fetchTweets = username => (dispatch) => {
   const uri = `http://localhost:3000/api/Tweets?filter={"where":{"username":"${username}"}}`
 
   fetch(uri)
@@ -17,10 +17,49 @@ const fetchTweets = (username) => (dispatch) => {
     }
     return response.json()
   })
-  .then((tweets) => dispatch(fetchTweetsSuccess(tweets)))
-  .catch((err) => console.error(err))
+  .then(tweets => dispatch(fetchTweetsSuccess(tweets)))
+  .catch(err => console.error(err))
+}
+
+const postTweetSuccess = (name, username, tweetText, timestamp) => ({
+  type: TWEET_POST_SUCCESS,
+  payload: {
+    name,
+    username,
+    tweetText,
+    timestamp,
+  },
+})
+
+const postTweet = (name, username, tweetText, token) => (dispatch) => {
+  const uri = 'http://localhost:3000/api/Tweets'
+
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      name,
+      username,
+      tweetText,
+    }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then(json => dispatch(
+    postTweetSuccess(json.name, json.username, json.tweetText, json.timestamp)
+  ))
+  .catch(err => console.error(err))
 }
 
 export {
   fetchTweets,
+  postTweet,
 }
