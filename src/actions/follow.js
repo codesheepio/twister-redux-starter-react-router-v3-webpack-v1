6e136d840d9547baa5856dc8062c2e12a19a8949
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-02 23:45:56 +0700

    Create toggleStatus thunk action

--- src/actions/follow.js
+++ src/actions/follow.js
@@ -1,4 +1,4 @@
-import { FOLLOW_FETCH_STATUS_SUCCESS } from './types'
+import { FOLLOW_FETCH_STATUS_SUCCESS, FOLLOW_TOGGLE_STATUS_SUCCESS } from './types'
 
 const fetchFollowStatusSuccess = (followedUsername, isFollowing) => ({
   type: FOLLOW_FETCH_STATUS_SUCCESS,
@@ -28,6 +28,39 @@ const fetchFollowStatus = (username, followedUsername) => (dispatch) => {
   .catch(err => console.error(err))
 }
 
+const toggleFollowStatusSuccess = (followedUsername, isFollowing) => ({
+  type: FOLLOW_TOGGLE_STATUS_SUCCESS,
+  payload: {
+    followedUsername,
+    isFollowing,
+  },
+})
+
+const toggleFollowStatus = (username, followedUsername, isFollowing, token) => (dispatch) => {
+  const uri = `http://localhost:3000/api/Follows/upsertWithWhere?where={"username":"${username}","followedUsername":"${followedUsername}"}`
+
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      isFollowing,
+    }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then(json => dispatch(toggleFollowStatusSuccess(json.followedUsername, json.isFollowing)))
+  .catch(err => console.error(err))
+}
+
 export {
-  fetchFollowStatus, // eslint-disable-line
+  fetchFollowStatus,
+  toggleFollowStatus,
 }
