4409db5b5f45bcf48128586fbdc6626c923b9488
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-02 22:30:45 +0700

    ProfileReducer handles fethcin follow status

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { PROFILE_FETCH_SUCCESS, TWEET_POST_SUCCESS } from '../actions/types'
+import { PROFILE_FETCH_SUCCESS, TWEET_POST_SUCCESS, FOLLOW_FETCH_STATUS_SUCCESS } from '../actions/types'
 
 const initialState = {
   name: '',
@@ -7,12 +7,14 @@ const initialState = {
   numFollowers: 0,
   numFollowings: 0,
   numTweets: 0,
+  isFollowing: false,
 }
 
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
     case PROFILE_FETCH_SUCCESS: {
       return {
+        ...state,
         name: action.payload.name,
         username: action.payload.username,
         email: action.payload.email,
@@ -25,6 +27,16 @@ const profileReducer = (state = initialState, action) => {
       return {
         ...state,
         numTweets: state.numTweets + 1,
+      }
+    }
+    case FOLLOW_FETCH_STATUS_SUCCESS: {
+      if (action.payload.followedUsername !== state.username) {
+        return state
+      }
+
+      return {
+        ...state,
+        isFollowing: action.payload.isFollowing,
       }
     }
     default: {
