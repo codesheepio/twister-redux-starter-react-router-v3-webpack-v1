cda96c89476c7f52652c9a6dbf7e769aaee66218
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-11-11 01:50:34 +0700

    Extract vendor bundle

---
+++ webpack.prod.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const webpack = require('webpack') // eslint-disable-line
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+module.exports = {
+  entry: {
+    app: path.resolve('client/index.js'),
+    vendors: ['react', 'react-dom', 'react-redux', 'react-router', 'redux', 'redux-form', 'redux-router', 'redux-thunk'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+    publicPath: '/dist/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel', 'webpack-module-hot-accept'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass'),
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('styles.css')
+  ],
+}
