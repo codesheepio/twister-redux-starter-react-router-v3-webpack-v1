6a902004f6e9cfb727944c66088052358ef4e43c
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-11-11 09:52:23 +0700

    Remove webpack-module-hot-accept

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -14,7 +14,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['babel', 'webpack-module-hot-accept'],
+        loaders: ['babel'],
       },
       {
         test: /\.scss$/,
